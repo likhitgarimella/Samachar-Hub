df88ca336f2ad20c4f92267ba90fbacffef9970f
Likhit Garimella <likhitgmella@gmail.com>
2021-05-12 03:39:01 +0530

    innovation vcs done

--- Samachar Hub/Innovation/MmViewController.swift
+++ Samachar Hub/Innovation/MmViewController.swift
@@ -18,7 +18,7 @@ class MmViewController: UIViewController {
         webview.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
         view.addSubview(webview)
         
-        let url = URL(string: "https://www.newsviewsnetwork.com/property/")!
+        let url = URL(string: "https://www.samacharhub.com/automobile/")!
         let request = URLRequest(url: url)
         webview.load(request)
         
--- Samachar Hub/Innovation/NnViewController.swift
+++ Samachar Hub/Innovation/NnViewController.swift
@@ -18,7 +18,7 @@ class NnViewController: UIViewController {
         webview.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
         view.addSubview(webview)
         
-        let url = URL(string: "https://www.newsviewsnetwork.com/food/")!
+        let url = URL(string: "https://www.samacharhub.com/biographies/")!
         let request = URLRequest(url: url)
         webview.load(request)
         
